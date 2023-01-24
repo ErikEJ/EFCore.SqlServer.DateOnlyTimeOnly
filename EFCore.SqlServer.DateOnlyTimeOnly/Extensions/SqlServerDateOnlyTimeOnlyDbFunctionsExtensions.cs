640431f0c5ee1ba27bdc85cee1f6b3af3cacc128
Erik Ejlskov Jensen <eej@delegate.dk>
2023-01-24 10:58:07 +0100

    add some more datediff translations

--- EFCore.SqlServer.DateOnlyTimeOnly/Extensions/SqlServerDateOnlyTimeOnlyDbFunctionsExtensions.cs
+++ EFCore.SqlServer.DateOnlyTimeOnly/Extensions/SqlServerDateOnlyTimeOnlyDbFunctionsExtensions.cs
@@ -23,11 +23,84 @@ namespace Microsoft.EntityFrameworkCore
         public static int? DateDiffDay(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
             => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
 
+        public static int DateDiffHour(this DbFunctions _, DateOnly startDate, DateOnly endDate)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffHour(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffMinute(this DbFunctions _, DateOnly startDate, DateOnly endDate)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffMinute(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffSecond(this DbFunctions _, DateOnly startDate, DateOnly endDate)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffSecond(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffMillisecond(this DbFunctions _, DateOnly startDate, DateOnly endDate)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffMillisecond(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffMicrosecond(this DbFunctions _, DateOnly startDate, DateOnly endDate)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffMicrosecond(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffNanosecond(this DbFunctions _, DateOnly startDate, DateOnly endDate)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffNanosecond(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
         public static int DateDiffWeek(this DbFunctions _, DateOnly startDate, DateOnly endDate)
            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
 
         public static int? DateDiffWeek(this DbFunctions _, DateOnly? startDate, DateOnly? endDate)
             => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffHour(this DbFunctions _, TimeOnly startTime, TimeOnly endTime)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffHour(this DbFunctions _, TimeOnly? startTime, TimeOnly? endTime)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffMinute(this DbFunctions _, TimeOnly startTime, TimeOnly endTime)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffMinute(this DbFunctions _, TimeOnly? startTime, TimeOnly? endTime)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffSecond(this DbFunctions _, TimeOnly startTime, TimeOnly endTime)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffSecond(this DbFunctions _, TimeOnly? startTime, TimeOnly? endTime)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffMillisecond(this DbFunctions _, TimeOnly startTime, TimeOnly endTime)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffMillisecond(this DbFunctions _, TimeOnly? startTime, TimeOnly? endTime)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffMicrosecond(this DbFunctions _, TimeOnly startTime, TimeOnly endTime)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffMicrosecond(this DbFunctions _, TimeOnly? startTime, TimeOnly? endTime)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int DateDiffNanosecond(this DbFunctions _, TimeOnly startTime, TimeOnly endTime)
+           => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
+        public static int? DateDiffNanosecond(this DbFunctions _, TimeOnly? startTime, TimeOnly? endTime)
+            => throw new NotImplementedException($"This method is available only for consuming via LINQ for EntityFramework translation to SQL.");
+
     }
 #pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
